7f93fe3a0339d3bbaa6d6c7a24af1eb5e22cf82d
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-03 21:42:07 +0900

    オブジェクトと変数の表示

---
+++ order.rb
@@ -0,0 +1,19 @@
+# order = "カフェラテ"
+# puts order
+# puts "ご注文は" + "カフェラテ" + "ですね？"
+# puts "カフェラテ" + "、オーダー入ります"
+# order = "カフェラテ"
+# puts "ご注文は" + order + "ですね？"
+# puts order + "、オーダー入ります"
+# puts "1 + 2の答えは#{1 + 2}"
+# order = "カフェラテ"
+# puts "ご注文は#{order}ですね？"
+# puts "ご注文は" + order + "ですね？"
+# x = gets
+# y = gets
+# puts x.to_i + y.to_i
+
+CaffeLatte = "カフェラテ"
+puts CaffeLatte
+CaffeLatte = "カフェラッテ"
+puts CaffeLatte
