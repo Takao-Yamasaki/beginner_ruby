b2727095d6305cf35cec9f9725dbf75ea39c4fd5
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 16:02:27 +0900

    要素を取得する

---
+++ drinks1.rb
@@ -0,0 +1,18 @@
+# drinks = ["コーヒー","カフェラッテ"]
+# puts drinks[0]
+# puts drinks[1]
+
+# drinks = ["コーヒー","カフェラッテ"]
+# puts drinks[-1]
+# puts drinks[-2]
+
+# drinks = ["コーヒー","カフェラッテ"]
+# puts drinks.first
+# puts drinks.last
+
+# drinks = ["コーヒー","カフェラッテ"]
+# p drinks[2]
+
+drinks = ["コーヒー","カフェラテ"]
+p drinks.first
+p ["コーヒー","カフェラテ"].first
