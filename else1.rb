8fc0a641c759c3f97137eb298e36af33a92be59b
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 12:47:00 +0900

    条件を満たさないときにも処理をする

---
+++ else1.rb
@@ -0,0 +1,26 @@
+  # wallet = 500
+  # if wallet >= 300
+  #   puts "コンビニでアイスを買って行こう！"
+  # else
+  #   puts "川沿いを散歩しよう！"
+  # end
+
+# season = "夏"
+# if season == "春"
+#   puts "アイスを買って行こう！"
+# elsif season == "夏"
+#   puts "かき氷を買って行こう！"
+# else
+#   puts "あんまん買って行こう！"
+# end
+
+season = "夏"
+if season == "春"
+    puts "アイスを買って行こう"
+else
+  if season == "夏"
+    puts "かき氷を買って行こう"
+  else
+    puts "あんまん買って行こう"
+  end
+end
