9bb6f63c36500c0bbc273e58f2515606dd337613
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 11:21:54 +0900

    chapter2の練習問題

---
+++ calc_tax.rb
@@ -0,0 +1,23 @@
+# puts 2 + 3
+
+# puts 2 * 2 * 3.14
+
+# puts "Ruby"
+
+# puts "abc" + "def"
+
+# puts "123".to_i + "456".to_i
+
+# coffee = 400
+# espresso = 100
+# total = coffee + espresso
+# puts "コーヒー:" + coffee.to_s + "円"
+# puts "合計:" + total.to_s + "円"
+
+# total = 300 * 2
+# p total
+# tax = total * 0.08
+# puts tax
+
+total = 300 * 2
+puts total
