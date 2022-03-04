35ccd954948c250384e01724cd2b4704f9fe5cb4
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-05 07:06:08 +0900

    配列の要素を並び替える

---
+++ sort1.rb
@@ -0,0 +1,11 @@
+# p [4, 2 , 8].sort
+
+# p ["hitomi", "achi", "tama"].sort
+# p ["aya", "achi", "tama"].sort
+# p ["aya", "achi", "Tama"].sort
+
+# p [4, 2, 8].reverse
+# p [4, 2, 8].sort.reverse
+
+p "abc".reverse
+p "とくれせんたぼーび".reverse
