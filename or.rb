8d090188223369f2add4bd6099c03b661235a734
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 12:57:16 +0900

    複数の条件を組み合わせる

---
+++ or.rb
@@ -0,0 +1,19 @@
+# wallet = 100
+# suica = 300
+
+# if wallet >= 300 || suica >= 300
+#   puts "コンビニでアイスを買って行こう"
+# end
+
+wallet = 500
+weather = "fine"
+if wallet >= 300 && weather == "fine"
+  puts "コンビニでアイスを買って行こう"
+end
+
+if 100
+  puts "100で成立しました"
+end
+if "abc"
+  puts "abcで成立しました"
+end
