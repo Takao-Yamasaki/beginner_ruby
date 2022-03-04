9af424da8fa015046ac0c90ccf9c6fef1acf5122
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 16:40:51 +0900

    配列をまとめて扱う

---
+++ each1.rb
@@ -0,0 +1,22 @@
+# drinks = ["コーヒー","カフェラッテ","モカ"]
+# drinks.each do |drink|
+#   puts drink
+# end
+
+# drinks = ["コーヒー","カフェラッテ"]
+# puts drinks[0]
+# puts drinks[1]
+
+# [1, 2, 3].each do |x|
+#   break if x == 2
+#   puts x
+# end
+
+# [1, 2, 3].each do |x|
+#   next if x == 2
+#   puts x
+# end
+
+(3..5).each do |x|
+  puts x  
+end
