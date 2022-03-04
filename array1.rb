d9795c5405e34defb295445bea54ad02fef731ac
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 15:53:32 +0900

    配列を代入する変数は複数形

--- array1.rb
+++ array1.rb
@@ -6,3 +6,11 @@
 
 drinks = ["カフェラテ","モカ","コーヒー"]
 p drinks
+
+# 配列を代入する変数は複数形にする
+sugars = ["白砂糖","黒砂糖","角砂糖"]
+p sugars
+coffee_beans = ["Brazil","Kenya","Nicaregua"]
+p coffee_beans
+even_numbers = [2, 4, 6]
+p even_numbers
