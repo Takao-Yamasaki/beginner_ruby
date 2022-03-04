b2fb5a9cb62431a1aaba8f061314a2b95d2b1369
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 11:37:09 +0900

    条件を判断する

---
+++ gtlt.rb
@@ -0,0 +1,25 @@
+# puts 1 < 2
+# puts 1 > 2
+
+# puts 1 <= 2
+# puts 2 >= 2
+
+# wallet = 500
+# puts wallet >= 300
+
+# wallet = 100
+# puts wallet >= 300
+
+# puts 1 == 2
+# puts 2 == 2
+# puts 2 == 1 + 1
+
+# puts 1 != 2
+# puts 2 != 2
+
+# puts "ruby" == "ruby"
+# puts "ruby" != "ruby"
+# puts "ruby" == "xxx"
+
+puts 2.even?
+puts 3.even?
