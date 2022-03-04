514e50fa77888d6439e7e39ac244544dd74fe354
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-05 08:23:27 +0900

    配列の各要素を変換する

---
+++ chapter5.rb
@@ -0,0 +1,32 @@
+# p ["コーヒー", "カフェラテ"].size
+
+# p [1, 2, 3, 4, 5].sum
+
+#  p ["モカ", "カフェラテ", "モカ"].uniq
+
+# str = "abc"
+# str.clear
+# p str
+
+# p ["カフェラテ","モカ","カプチーノ"].sample
+
+# p ["大吉", "中吉", "末吉", "凶"].sample
+
+# p [100, 50, 300].sort
+
+# p [100, 50, 300].sort.reverse
+
+# p "cba".reverse
+
+# p ["100", "50", "300"].join(",")
+
+# p "100,50,300".split(",")
+
+# p [1, 2, 3].map{|n| n * 3}
+
+# p ["abc", "xyz"].map(&:reverse)
+
+# result = ["aya", "aichi", "Tama"].map(&:downcase)
+# p result.sort
+
+p ["aya", "aichi", "Tama"].map{|text| text.downcase}.sort
