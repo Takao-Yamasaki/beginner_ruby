f01434a92009dbee33fcaf97543ccd6dfca1925e
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-04 17:01:01 +0900

    chapter4の練習問題

---
+++ chapter4.rb
@@ -0,0 +1,36 @@
+# p ["コーヒー", "カフェラテ"]
+
+# drinks = ["コーヒー", "カフェラテ", "モカ"]
+# p drinks[1]
+# p drinks.first
+# p drinks.last
+
+# drinks = ["コーヒー", "カフェラテ"]
+# drinks.push("モカ")
+# p drinks
+
+# num = [2, 3]
+# num.unshift(1)
+# p num
+
+# p [1, 2] + [3, 4]
+
+# drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+
+# drinks.each do |drink|
+#   p drink
+# end
+
+# drinks = ["ティーラテ", "カフェラテ", "抹茶ラテ"]
+
+# drinks.each do |drink|
+#   p drink + "でお願いします"
+# end
+
+nums = []
+
+sum = 0
+nums.each do |num|
+  sum += num
+end
+p sum
