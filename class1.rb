bac96a95f9f54dd80fb2c86ec3e7aebe427ae8b5
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-05 15:11:30 +0900

    クラスとは

---
+++ class1.rb
@@ -0,0 +1,27 @@
+# p 1.class
+# p 2.class
+# p 100.class
+
+# p "カフェラテ".class
+# p "hello".class
+# p "".class
+
+# p [1, 2, 3].class
+# p ["コーヒー", "カフェラテ"].class
+# p [].class
+
+# 偶数かどうか判断する
+# p 1.even?
+# p 2.even?
+# p 100.even?
+
+# p 1.even?
+#p "カフェラテ".even?
+
+# p "カフェラテ"
+# p [1, 2, 3]
+
+p String.new
+p String.new("カフェラテ")
+p Array.new
+p Array.new(2, "カフェラテ")
