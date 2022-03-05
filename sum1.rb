c7f44a4a923738e4de873d741d932c02ba76e9aa
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-05 11:43:34 +0900

    メソッドを作って呼び出す

---
+++ sum1.rb
@@ -0,0 +1,18 @@
+# a = [1, 2, 3]
+# puts a.sum
+
+# a = [1, 2, 3]
+# sum = 0
+# a.each do |x|
+#   sum += x
+# end
+# puts sum
+
+# puts 2 * 2
+
+def area
+  puts 2 * 2
+end
+
+result = area
+puts result
