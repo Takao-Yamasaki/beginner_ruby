4e30ecb87bd552fa06127509621ab5122c2e2221
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-05 09:17:44 +0900

    オブジェクトを組で使う

---
+++ hash1.rb
@@ -0,0 +1,13 @@
+# p ({:coffee => 300, :caffe_latte => 400})
+
+# p "coffee".to_sym
+# p :coffee.to_s
+
+# menu = {:coffee => 300, :caffe_latte => 400}
+# p menu
+
+# menu = {coffee: 300, caffe_latte: 400}
+# p menu[:coffee]
+
+book = {title: "Ruby Book", memders: ["yano","beco"]}
+p book[:memders]
