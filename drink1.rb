cbffd47b96ceb2fb8b562a33ea7be3003cde28da
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-06 11:00:49 +0900

    部品を別ファイルに分ける

--- drink1.rb
+++ drink1.rb
@@ -1,5 +1,20 @@
+# class Drink
+# end
+
+# drink = Drink.new
+# p drink.class
+
+require_relative "whipped_cream"
 class Drink
+  include WhippedCream
+  def name
+    @name
+  end
+  def initialize
+    @name = "モカ"
+  end
 end
 
-drink = Drink.new
-p drink.class
+mocha = Drink.new
+mocha.whipped_cream
+puts mocha.name
