387a9ac1e320ac1543491949a80573e004079926
Takao-Yamasaki <captain_kaeru@yahoo.co.jp>
2022-03-06 10:15:14 +0900

    モジュールのメソッドや定数をそのまま使う

---
+++ module_method.rb
@@ -0,0 +1,32 @@
+# module WhippedCream
+#   def self.info
+#     "トッピング用ホイップクリーム"
+#   end
+# end
+# puts WhippedCream.info
+
+# module WhippedCream
+#   Price = 100
+# end
+# puts WhippedCream::Price
+
+# puts Math::PI
+# puts Math.cos(Math::PI)
+
+module BecoCafe
+  class Coffee
+    def self.info
+      "深みと香りのコーヒー"
+    end
+  end
+end
+
+module MachuCafe
+  class Coffee
+    def self.info
+      "豊かな甘みのコーヒー"
+    end
+  end
+end
+puts BecoCafe::Coffee.info
+puts MachuCafe::Coffee.info
